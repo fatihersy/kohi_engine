eb533896a00a6a5c18495518ed944e9bf2b3139f
fatihersy <fatih.r.ersoy@gmail.com>
2023-12-22 23:51:31 +0300

    Math library Part 2. Video 25. 22/12/23

--- engine/src/math/math_types.h
+++ engine/src/math/math_types.h
@@ -17,34 +17,29 @@ typedef union vec2_u {
     };
 } vec2;
 
-typedef struct vec3_u {
-    union {
-        // An array of x, y, z
-        f32 elements[3];
-        struct {
-            union {
-                // The first element.
-                f32 x, r, s, u;
-            };
-            union {
-                // The second element.
-                f32 y, g, t, v;
-            };
-            union {
-                // The third element.
-                f32 z, b, p, w;
-            };
+typedef union vec3_u {
+    // An array of x, y, z
+    f32 elements[3];
+    struct {
+        union {
+            // The first element.
+            f32 x, r, s, u;
+        };
+        union {
+            // The second element.
+            f32 y, g, t, v;
+        };
+        union {
+            // The third element.
+            f32 z, b, p, w;
         };
     };
 } vec3;
 
 typedef union vec4_u {
-#if defined(KUSE_SIMD)
-    // Used for SIMD operations.
-    alignas(16) __m128 data;
-#endif
+
     // An array of x, y, z, w
-    alignas(16) f32 elements[4];
+    f32 elements[4];
     union {
         struct {
             union {
@@ -65,7 +60,10 @@ typedef union vec4_u {
             };
         };
     };
-
 } vec4;
 
 typedef vec4 quat;
+
+typedef union mat4_u {
+    f32 data[16];
+} mat4;
